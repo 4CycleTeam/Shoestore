321036fe5349668279a142c8a2ad5115ad1c0b70
Ximena Campo <ximenacampol@gmail.com>
2022-10-12 21:42:26 -0500

    configuracion lanzamiento de servidor

---
+++ back/server.js
@@ -0,0 +1,11 @@
+const app=require("./app")
+
+//Setear el archivo de configuración
+const dotenv=require("dotenv");
+dotenv.config({path: 'back/config/config.env'})
+
+
+//Llamar al servidor
+const server=app.listen(process.env.PORT, () => {
+    console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
+})
