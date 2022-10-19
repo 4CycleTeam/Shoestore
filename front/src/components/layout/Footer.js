3d2ada30311185885b8ac8ce8aaa48fe76ee5700
Mariana Reyes Tique <mareti18@gmail.com>
2022-10-18 21:49:06 -0500

    Se ajusta el footer con clases

--- front/src/components/layout/Footer.js
+++ front/src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 export const Footer = () => {
   return (
     <Fragment>
-        <footer className="py-1">
+        <footer className="footer">
             <p className="text-center mt-1">
                 Shoes Store - 2022 - Mision TIC - Todos los derechos reservados
             </p>
