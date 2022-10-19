a0a4962a7b1727deba89157d499c97ddd245daea
Isaura <110794045+IsaEckar@users.noreply.github.com>
2022-10-19 06:23:00 -0500

    icono favicon

--- back/app.js
+++ back/app.js
@@ -1,4 +1,22 @@
-const express=require("express");
-const app = express();
+import './App.css';
+import React from 'react';
+import Header from './components/layout/Header';
 
-module.exports=app
+//Router traido desde react-router-dom (no confundir con el de express)
+//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <Router>
+  <div className="App">
+        <Header />
+    
+        
+        <Footer />
+    </div>
+    </Router>
+  );
+}
+
+export default App;
