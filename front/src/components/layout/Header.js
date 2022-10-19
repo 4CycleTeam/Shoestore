d3d67abf876e5d88a16a2f4feb72bb1a4055ee7c
julianguapacha@hotmail.com <julianguapacha@hotmail.com>
2022-10-19 10:46:50 -0500

    Cambio el background del header

--- front/src/components/layout/Header.js
+++ front/src/components/layout/Header.js
@@ -5,14 +5,14 @@ const Header = () => {
 
     return (
         <Fragment>
-            <nav className='navbar row'>
+            <nav className='navbar row bg-dark' >
                 <div className='col-12 col-md-3'>
-                    <div className='navbar-brand'>
+                    <div className='navbar-brand' >
                         <img src="./images/ShoestoreLogo.png" alt="Shoes Store Logo"></img>
                     </div>
                 </div>
 
-                <div className='col-12 col-md-6 mt-2 mt-md-0'>
+                <div className='col-12 col-md-4 mt-2 mt-md-0'>
                     <div className="input-group">
                         <input
                             type="text"
@@ -27,7 +27,7 @@ const Header = () => {
                     </div>
                 </div>
                 <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-                    <span><button className='btn' id="login_btn">Inicie Sesión</button></span>
+                    <span><button className='btn text-white' id="login_btn">Inicie Sesión</button></span>
                     &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
                     <i class="fa fa-shopping-cart fa-2x text-white" aria-hidden="false"></i>
                     <span className="ml-1" id="cart_count">2</span>
