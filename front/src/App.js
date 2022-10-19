e0faa5b8e7d1db09bb17bdf2bc290ca66d1c1e9d
Mariana Reyes Tique <mareti18@gmail.com>
2022-10-18 20:53:50 -0500

    Se ajustar el footer del proyecto

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 import React from 'react';
-<<<<<<< HEAD
-=======
 import Header from './components/layout/Header';
-
->>>>>>> 8c580ada391a7c034a97beffdb37e514c4f5209e
+import { Footer } from './components/layout/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
 
+      <Footer />
     </div>
   );
 }
