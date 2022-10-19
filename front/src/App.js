8c580ada391a7c034a97beffdb37e514c4f5209e
IvanB3 <ivanbenavides098@gmail.com>
2022-10-18 20:32:27 -0500

    Cambios en index.html

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,13 @@
-
 import './App.css';
+import React from 'react';
+import Header from './components/layout/Header';
+
 
 function App() {
   return (
     <div className="App">
-   
+      <Header/>
+
     </div>
   );
 }
