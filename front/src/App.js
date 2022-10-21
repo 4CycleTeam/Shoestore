92adfb82095b68d74b01184ac283754e82b81f51
julianguapacha@hotmail.com <julianguapacha@hotmail.com>
2022-10-20 22:05:42 -0500

    Se agrega navbar a header y ventas.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import Header from './components/layout/Header';
+import Ventas from './components/Ventas';
 import { Footer } from './components/layout/Footer';
 import Home from './components/Home';
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <Home />
+      <Ventas />
 
       <Footer />
     </div>
---
+++ front/src/components/Ventas.js
@@ -0,0 +1,46 @@
+import React, { Fragment} from 'react'
+
+export const Home = () => {
+
+
+    return (
+        <Fragment>
+            <h1 id="encabezado_Ventas">Ventas</h1>
+
+            <section id="productos" className='container mt-5'>
+            <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table> 
+            </section>
+
+        </Fragment>
+    )
+}
+export default Home
