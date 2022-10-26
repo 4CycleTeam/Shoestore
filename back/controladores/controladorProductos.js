793400c8146e39fd01866d8988b6833ed7944b61
Ximena Campo <32852877+Ximec@users.noreply.github.com>
2022-10-26 02:16:00 -0500

    Finalizado backend del producto con el CRUD

--- back/controladores/controladorProductos.js
+++ back/controladores/controladorProductos.js
@@ -1,12 +1,13 @@
 const producto=require("../modelos/productos")
-//const fetch =(url)=>import('node-fetch').then(({default:fetch})=>fetch(url)); 
+const fetch =(url)=>import('node-fetch').then(({default:fetch})=>fetch(url)); 
 
 //Ver la lista de productos
-exports.getProducts=(req,res,next) =>{
-    //const productos= await producto.find();
+exports.getProducts=async (req,res,next) =>{
+    const productos= await producto.find();
     res.status(200).json({
         success:true,
-        message:"en esta ruta usted va a ver todos los productos"
+        cantidad: productos.length,
+        productos
     })
 }
 
@@ -20,79 +21,79 @@ exports.newProduct=async(req,res,next)=>{
     })
 }
 
-// //Ver un producto por ID
-// exports.getProductById= async (req, res, next)=>{
-//     const product= await producto.findById(req.params.id)
+//Ver producto por ID
+exports.getProductById= async (req, res, next)=>{
+    const product= await producto.findById(req.params.id)
     
-//     if (!product){
-//             return res.status(404).json({
-//             success:false,
-//             message: 'No encontramos ese producto'
-//         })
-//     }
-//     res.status(200).json({
-//         success:true,
-//         message:"Aqui debajo encuentras información sobre tu producto: ",
-//         product
-//     })
-// }
+    if (!product){
+            return res.status(404).json({
+            success:false,
+            message: 'No se encuentra el producto solicitado'
+        })
+    }
+    res.status(200).json({
+        success:true,
+        message:"Debajo encuentra información sobre su producto: ",
+        product
+    })
+}
 
-// //Update un producto
-// exports.updateProduct= async (req,res,next) =>{
-//     let product= await producto.findById(req.params.id) //Variable de tipo modificable
-//     if (!product){ //Verifico que el objeto no existe para finalizar el proceso
-//             return res.status(404).json({
-//             success:false,
-//             message: 'No encontramos ese producto'
-//         })
-//     }
-//     //Si el objeto si existia, entonces si ejecuto la actualización
-//     product= await producto.findByIdAndUpdate(req.params.id, req.body, {
-//         new:true, //Valido solo los atributos nuevos o actualizados
-//         runValidators:true
-//     });
-//     //Respondo Ok si el producto si se actualizó
-//     res.status(200).json({
-//         success:true,
-//         message:"Producto actualizado correctamente",
-//         product
-//     })
-// }
+//Actualizar producto
+exports.updateProduct= async (req,res,next) =>{
+    let product= await producto.findById(req.params.id) //Variable de tipo modificable
+    if (!product){ //Verificacion de la existencia del producto
+            return res.status(404).json({
+            success:false,
+            message: 'No se encuentra el producto'
+        })
+    }
+    //Se ejecuta actualización si existe el producto
+    product= await producto.findByIdAndUpdate(req.params.id, req.body, {
+        new:true, //Valido solo los atributos nuevos o actualizados
+        runValidators:true
+    });
+    //Respondo Ok si el producto si se actualizó
+    res.status(200).json({
+        success:true,
+        message:"Producto actualizado correctamente",
+        product
+    })
+}
 
 
-// //Eliminar un producto
-// exports.deleteProduct= async (req,res,next) =>{
-//     const product= await producto.findById(req.params.id) //Variable de tipo modificable
-//     if (!product){ //Verifico que el objeto no existe para finalizar el proceso
-//             return res.status(404).json({ //Si el objeto no existe, return termina el metodo
-//             success:false,
-//             message: 'No encontramos ese producto'
-//         })
-//     }
+//Eliminar un producto
+exports.deleteProduct= async (req,res,next) =>{
+    const product= await producto.findById(req.params.id) //Variable de tipo modificable
+    if (!product){ //Verificacion de la existencia del producto
+            return res.status(404).json({ //Si el producto no existe se ternima el proceso de eliminación
+            success:false,
+            message: 'No se encuentra el producto'
+        })
+    }
 
-//     await product.remove();//Eliminamos el proceso
-//     res.status(200).json({
-//         success:true,
-//         message:"Producto eliminado correctamente"
-//     })
-// }
-// //HABLEMOS DE FETCH
-// //Ver todos los productos
-// function verProductos(){
-//     fetch('http://localhost:4000/api/productos')
-//     .then(res=>res.json())
-//     .then(res=>console.log(res))
-//     .catch(err=>console.error(err))
-// }
+    await product.remove();//Eliminacion del producto
+    res.status(200).json({
+        success:true,
+        message:"Producto eliminado correctamente"
+    })
+}
+// FETCH
+//Ver todos los productos
+function verProductos(){
+    fetch('http://localhost:4000/api/productos')
+    .then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err))
+}
 
-// //verProductos(); LLamamos al metodo creado para probar la consulta
+//verProductos(); LLamamos al metodo creado para probar la consulta
 
-// //Ver por id
-// function verProductoPorID(id){
-//     fetch('http://localhost:4000/api/producto/'+id)
-//     .then(res=>res.json())
-//     .then(res=>console.log(res))
-//     .catch(err=>console.error(err))
-// }
+//Ver por id
+function verProductoPorID(id){
+    fetch('http://localhost:4000/api/producto/'+id)
+    .then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err))
+}
 
-// //verProductoPorI
+//verProductoPorID('63456a8d9163cb9dbbcaa235'); Probamos el metodo con un id
--- back/rutas/rutasProductos.js
+++ back/rutas/rutasProductos.js
@@ -1,12 +1,12 @@
 const express=require("express")
 const router=express.Router();
 
-const {getProducts, newProduct} = require("../controladores/controladorProductos") //Traemos la respuesta json desde el controlador
+const {getProducts, newProduct, getProductById, updateProduct, deleteProduct} = require("../controladores/controladorProductos") //Respuesta json desde el controlador
 
-router.route('/productos').get(getProducts)  //Establecemos desde que ruta queremos ver el getProducts
-router.route('/producto/nuevo').post(newProduct); //establecemos la ruta
-// router.route('/producto/:id').get(getProductById); //Ruta para consultar por id
-// router.route('/producto/:id').put(updateProduct);//Creacion de la ruta de actualizacion
-// router.route('/producto/:id').delete(deleteProduct); //Creacion de la ruta de eliminacion por id
+router.route('/productos').get(getProducts)  //Ruta ver productos
+router.route('/producto/nuevo').post(newProduct); //Ruta crear producto
+router.route('/producto/:id').get(getProductById); //Ruta consulta por id
+router.route('/producto/:id').put(updateProduct);//Ruta Actualización
+router.route('/producto/:id').delete(deleteProduct); //Ruta eliminación
 
 module.exports=router;
